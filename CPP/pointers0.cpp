89c3f727e39247dd95996dd437da8cc6e9f0b0f9
Anubhav Sharma <anubhavsharma9996@gmail.com>
2019-11-16 19:15:29 +0530

    cpp pointers

--- CPP/pointers0.cpp
+++ CPP/pointers0.cpp
@@ -1,19 +1,16 @@
 #include<iostream>
-using namespace std;
 
 int main(){
 	int number = 10;
-	cout << "Value of number : " << number << " Address of number : " << &number << endl;
+	std::cout << "Value of number : " << number << " Address of number : " << &number << std::endl;
 
 	int *ptr1;
 	ptr1 = &number;
-	cout << "Value of *ptr1 : " << *ptr1 << " Value of ptr1 " << ptr1 << " Value of &ptr1 : " << &ptr1 << endl;
+	std::cout << "Value of *ptr1 : " << *ptr1 << " Value of ptr1 " << ptr1 << " Value of &ptr1 : " << &ptr1 << std::endl;
 
-
-	int *ptr2 = NULL;
+	int *ptr2 = NULL;		// heap memory allocated
 	ptr2 = new int;
-	cout << " Value of ptr2 " << ptr2 <<endl; 
 	ptr2 = &number;
-	cout << "Value of *ptr2 : " << *ptr2 << " Value of ptr2 " << ptr2 << " Value of &ptr2 : " << &ptr2 << endl;
+	std::cout << "Value of *ptr2 : " << *ptr2 << " Value of ptr2 " << ptr2 << " Value of &ptr2 : " << &ptr2 << std::endl;
 	return 0;
 }
